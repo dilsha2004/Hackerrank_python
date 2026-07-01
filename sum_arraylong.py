b1da675f8e581a92a67323eca0a974b28efe71a4
dilsha2004 <dilshajelish2004@gmail.com>
2026-07-01 13:55:18 +0530

    sum of elements of array

---
+++ sum_arraylong.py
@@ -0,0 +1,7 @@
+def aVeryBigSum(ar):
+    return sum(ar)
+if __name__ == '__main__':
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = aVeryBigSum(ar)
+    print(result)
