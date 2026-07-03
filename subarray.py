e7ed879393f45f65e008d54afd93d1507e5270af
dilsha2004 <dilshajelish2004@gmail.com>
2026-07-03 12:52:37 +0530

    Two children, Lily and Ron, want to share a chocolate bar. Each of the squares has an integer on it.

---
+++ subarray.py
@@ -0,0 +1,16 @@
+def birthday(s, d, m):
+    count = 0
+    for i in range(len(s)-m+1):
+        parts = s[i:i+m]
+        if sum(parts) == d:
+            count +=1
+    return count
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    s = list(map(int, input().rstrip().split()))
+    first_multiple_input = input().rstrip().split()
+    d = int(first_multiple_input[0])
+    m = int(first_multiple_input[1])
+    result = birthday(s, d, m)
+    print(result)
